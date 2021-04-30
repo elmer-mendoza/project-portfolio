75919534a89fd68e5aba1ce4e99714823b89ad4d
Elmer Mendoza <mdelefm@gmail.com>
2021-04-30 02:58:06 -0600

    create structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header"
+import Hero from "./Components/Hero"
+import Projects from "./Components/Projects"
+import Skills from "./Components/Skills"
+import Resume from "./Components/Resume"
+import Reviews from "./Components/Reviews"
+import Footer from "./Components/Footer"
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Hero/>
+      <Projects/>
+      <Skills/>
+      <Resume/>
+      <Reviews/>
+      <Footer/>
+     
+    </>
   );
 }
 
